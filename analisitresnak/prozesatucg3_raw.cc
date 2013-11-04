df5909af46fe1541445bdfe598086b717a5a5d45
nerea <nerea@b69dd40d-ea26-429e-b172-635707864a66>
2013-11-04 08:43:22 +0000

    Esaldika desanbiguatu

--- analisitresnak/prozesatucg3_raw.cc
+++ analisitresnak/prozesatucg3_raw.cc
@@ -18,7 +18,7 @@ using namespace pcrepp;
 
 string getEnvVar(string const& key);
 string kendu_cg3rako_etiketak(string info);
-void PrintResults (list < sentence > &ls, int level, string &oinIzen);
+void PrintResults (list < sentence > &ls, int level);
 void desHMM(int maila, string &desIrteera, string &oinIzen);
 
 
@@ -81,114 +81,107 @@ int prozesatuCG3Raw(int maila, string &oinIzen) {
 
 void desHMM(int maila, string &desIrteera,string &oinIzen) {
   euParole parolera(maila);
-    list<sentence> ls;
-    sentence lws;
-    ifstream in(desIrteera.c_str());
-    string sars;
-    string tmpVar = getEnvVar("IXA_PREFIX");
-    string tmpName;
+  list<sentence> ls;
+  sentence lws;
+  ifstream in(desIrteera.c_str());
+  string sars;
+  string tmpVar = getEnvVar("IXA_PREFIX");
+  string tmpName;
+  
+  if (tmpVar.length()>0) {
+    tmpVar += "/var/mg/";
+    tmpName = tmpVar + "eustaggerhmmf.dat";
+  }
+  else {
+    cerr << "prozesatuCG3Raw => ERRORE LARRIA: 'IXA_PREFIX' ingurune aldagaia ezin daiteke atzitu" << endl;
+    exit(EXIT_FAILURE);
+  }
+  wstring tagDat = util::string2wstring(tmpName);
+  hmm_tagger taggerEu(L"eu",tagDat, false, FORCE_TAGGER,1);
+  stringstream irt;
+  
+  if (!in) {
+    exit(EXIT_FAILURE);
+  }
 
-    if (tmpVar.length()>0) {
-      tmpVar += "/var/mg/";
-      tmpName = tmpVar + "eustaggerhmmf.dat";
+  getline(in,sars);
+  while (sars.length()>0) {
+    string formL;
+    wstring form,lemma,tag;
+    Pcre firstL("\\\"<\\$?(.[^>]*)>\\\"");
+    if (firstL.search(sars)) {
+      string formTmp = firstL.get_match(0);
+      form = util::string2wstring(formTmp);
     }
-    else {
-      cerr << "prozesatuCG3Raw => ERRORE LARRIA: 'IXA_PREFIX' ingurune aldagaia ezin daiteke atzitu" << endl;
-      exit(EXIT_FAILURE);
-    }
-    wstring tagDat = util::string2wstring(tmpName);
-    hmm_tagger taggerEu(L"eu",tagDat, false, FORCE_TAGGER,1);
-
-    if (!in) {
-      exit(EXIT_FAILURE);
-    }
+    word w(form);
+    sars.clear();
     getline(in,sars);
-    while (sars.length()>0) {
-      string formL;
-      wstring form,lemma,tag;
-      Pcre firstL("\\\"<\\$?(.[^>]*)>\\\"");
-      if (firstL.search(sars)) {
-	string formTmp = firstL.get_match(0);
-	form = util::string2wstring(formTmp);
+    while (sars.length()>0 && sars[0] != '"') {
+      parolera.getLemmaTag(sars,lemma,tag);
+      if (lemma.length()>0) {
+	analysis an(lemma,tag);
+	w.add_analysis(an);
       }
-      word w(form);
-      // wstring wForm = util::string2wstring(form);
-      // w.set_form(wForm);
+      else if (tag.length()>0) {
+	lemma = form;
+	analysis an(lemma,tag);
+	w.add_analysis(an);
+      }
       sars.clear();
       getline(in,sars);
-      while (sars.length()>0 && sars[0] != '"') {
-	//	sars = kendu_cg3rako_etiketak(sars);
-	parolera.getLemmaTag(sars,lemma,tag);
-	// analysis an(util::string2wstring(lemma),util::string2wstring(tag));
-	if (lemma.length()>0) {
-	  analysis an(lemma,tag);
-	  w.add_analysis(an);
-	}
-	else if (tag.length()>0) {
-	  lemma = form;
-	  analysis an(lemma,tag);
-	  w.add_analysis(an);
-	}
-	sars.clear();
-	getline(in,sars);
-      }
-      //set prob
-      int anNum = w.get_n_analysis();
-      for (word::iterator an = w.analysis_begin();an != w.analysis_end();an++) {
-	an->set_prob(1/anNum);
-      }
-      
-      lws.push_back(w);
-      if (form == L"." || form == L"!" || form == L"?") {
-	ls.push_back(lws);
-	lws.clear();
-      }
-      if (sars[0] != '"') {
-	sars.clear();
-	getline(in,sars);
-      }
+    }
+    //set prob
+    int anNum = w.get_n_analysis();
+    for (word::iterator an = w.analysis_begin();an != w.analysis_end();an++) {
+      an->set_prob(1/anNum);
     }
     
-    if (lws.size() > 0) {
+    lws.push_back(w);
+    if (form == L"." || form == L"!" || form == L"?") {
       ls.push_back(lws);
       lws.clear();
-    }
-    if (ls.size() > 0) {
       taggerEu.analyze(ls);
-      PrintResults(ls,maila,oinIzen);
+      PrintResults(ls,maila);
       ls.clear();
-    }  
+    }
+    if (sars[0] != '"') {
+      sars.clear();
+      getline(in,sars);
+    }
+  }
+  
+  if (lws.size() > 0) {
+    ls.push_back(lws);
+    lws.clear();
+  }
+  if (ls.size() > 0) {
+    taggerEu.analyze(ls);
+    PrintResults(ls,maila);
+    ls.clear();
+  }  
 }
-void PrintResults (list < sentence > &ls, int level, string &oinIzen) {
+void PrintResults (list < sentence > &ls, int level) {
   sentence::const_iterator w;
   list < sentence >::iterator si;
-  stringstream irt;
  
-  if (level != 0) 
-    irt << oinIzen << ".etiketatua" << level;
-  else
-    irt << oinIzen << ".morfeus";
-  string irtIzen = irt.str();
-  wofstream taggedOut(irtIzen.c_str());
-
   for (si = ls.begin (); si != ls.end (); si++) {
-
-      for (w = si->begin (); w != si->end (); w++) {
-	taggedOut << w->get_form ();
-	if (level > 0) {
-	  for (word::const_iterator an = w->selected_begin (); an != w->selected_end (); an++) {
-	    taggedOut << L" " << an->get_lemma () << L" " << an->get_tag () ;
-	  }
-	  taggedOut << endl;
+    for (w = si->begin (); w != si->end (); w++) {
+      wcout << w->get_form ();
+      if (level > 0) {
+	for (word::const_iterator an = w->selected_begin (); an != w->selected_end (); an++) {
+	  wcout << L" " << an->get_lemma () << L" " << an->get_tag () ;
 	}
-	else {
-	  for (word::const_iterator an = w->analysis_begin();an != w->analysis_end();an++) {
-	    taggedOut << L" " << an->get_lemma () << L" " << an->get_tag () ;
-	  }
-	  taggedOut << endl;
-	}
+	wcout << endl;
       }
+      else {
+	for (word::const_iterator an = w->analysis_begin();an != w->analysis_end();an++) {
+	  wcout << L" " << an->get_lemma () << L" " << an->get_tag () ;
+	}
+	wcout << endl;
+      }
+    }
     // sentence separator: blank line.
-    taggedOut << endl;
+    wcout << endl;
   }
+  wcout.flush();
 }
